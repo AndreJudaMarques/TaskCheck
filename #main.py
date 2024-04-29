615bbf8666b7a35fa7d9a6434ccb1132f6f0b620
André Judá <judatrainer@gmail.com>
2024-04-29 12:51:03 -0300

    inicio

--- #main.py
+++ #main.py
@@ -1,1 +1,16 @@
-#main
+# importar o App,
+# importar Builder (GUI), interface
+# criar o app
+# criar a função build
+
+from kivy.app import App
+
+from kivy.lang import Builder
+
+GUI = Builder.load_file("tela.kv")
+
+
+
+class MeuAplicativo(App):
+      def build(self):
+            return GUI
